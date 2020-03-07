509d856a7e87185c42157c5880db5f76e16c35eb
Jean-Michel Saulnier <jmsaulnier.dev@gmail.com>
2020-03-06 20:52:06 -0800

    🚀

---
+++ src/App.svelte
@@ -0,0 +1,22 @@
+<script>
+	let src = 'elastos.png'
+</script>
+
+<main>
+	<img src={src} width="200px" height="auto" alt="Elastos">
+</main>
+
+<style>
+	main {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		height: 100%;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
