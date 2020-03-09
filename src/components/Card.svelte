f5563293f84eedbf72a954d3c7ff7ddf226753b4
Jean-Michel Saulnier <jmsaulnier.dev@gmail.com>
2020-03-08 22:06:41 -0700

    add dApp list

---
+++ src/components/Card.svelte
@@ -0,0 +1,54 @@
+<script>
+	export let dapp;
+</script>
+
+<style>
+	a {
+		display: flex;
+		margin: 16px 8px;
+		text-decoration: none;
+		color: black;
+	}
+
+	@media screen and (min-width: 35em) {
+		a {
+			margin: 32px 16px;
+		}
+	}
+
+	img {
+		flex-shrink: 0;
+    background-color: white;
+    display: block;
+    max-width: 100%;
+    height: 64px;
+    width: 64px;
+    box-shadow: rgba(0, 0, 0, 0.1) 1px 0px 16px 0px;
+    border-radius: 16px;
+	}
+
+	div {
+		margin-left: 16px;
+	}
+
+	h3 {
+		margin-bottom: 4px;
+    font-size: 16px;
+    line-height: 1.45;
+    font-weight: 600;
+    display: inline-block;
+	}
+
+	h4 {
+		font-weight: 100;
+		font-size: 14px;
+	}
+</style>
+
+<a href={`https://scheme.elastos.org/app?id=${dapp.packageName}`} target="_blank">
+	<img src={`https://dapp-store.elastos.org/apps/${dapp._id}/icon`} alt={dapp.appName} />
+	<div>
+		<h3>{dapp.appName}</h3>
+		<h4>{dapp.appShortDescription || dapp.appDescription}</h4>
+	</div>
+</a>
--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,18 +1,32 @@
 <style>
 	main {
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		height: calc(100vh - 25px);
+		margin-left: auto;
+    margin-right: auto;
+    width: 100%;
+		max-width: 1280px;
+		font-family: 'Lato', sans-serif;
 	}
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
+	header {
+		display: flex;
+		justify-content: center;
+		align-content: center;
+		text-align: center;
+		padding: 32px;
+		border-bottom: 1px solid rgb(240, 240, 240);
+		font-size: 16px;
+		line-height: 16px;
+		font-family: 'Lato', sans-serif;
+	}
+
+	header img {
+		margin-left: 8px;
 	}
 </style>
 
+<header>
+	<em>powered by</em> <a href="https://www.elastos.org/" target="_blank"><img src="elastos.png" width="auto" height="20" alt="Elastos" /></a>
+</header>
 <main>
 	<slot></slot>
 </main>
